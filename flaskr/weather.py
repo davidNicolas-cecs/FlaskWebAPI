9f4191ba20fc8372af8bc0a5618993f02f9152fc
davidNicolas-cecs <david.nicolasgarcia@student.csulb.edu>
2023-12-28 22:02:52 -0800

    Updated weather site to include up to data weather for City Name, and updated weather.html and weather.py

--- flaskr/weather.py
+++ flaskr/weather.py
@@ -19,13 +19,15 @@ def index():
     db=get_db
     if request.method == 'POST':
         city = request.form['city']
+        #everytime we search something that gets a hit, we send it to the database
         state_code = request.form['state_code']
-        data = weather_finder(city,state_code=state_code)
-        print(data)
+        data = geocoding(city,state_code=state_code)
         if data is not None:
+            print("DATA IS:",data)
+            #0 - name, 1 - temp, 2 - feels like, 3 - pressure, 4 - humidity
             #if true redirect to weather_displayer function
             
-            return redirect(url_for('weather.weather_displayer',long=data[0],lat=data[1]))
+            return redirect(url_for('weather.weather_displayer', name=data[0], temp=data[1], feels_like=data[2], pressure=data[3], humidity=data[4]))
 
         
         
@@ -34,11 +36,8 @@ def index():
 
 
 
-def weather_finder(city,country_code="",state_code="",limit=1):
-    API_key = "696ff3236cff2b889401a9a8d64c86c2"
-    
-
-        
+def geocoding(city,country_code="",state_code="",limit=1):
+    API_key = "696ff3236cff2b889401a9a8d64c86c2"    
     url = f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state_code},{country_code}&limit={limit}&appid={API_key}"
     response = requests.get(url)
     error = ""
@@ -49,12 +48,12 @@ def weather_finder(city,country_code="",state_code="",limit=1):
 
         # Access data 
         first_item = data_list[0]
+        name = first_item['name']
         lat = first_item["lat"]
         long = first_item["lon"]
+        data.append(name)
+        current_weather(lat,long,API_key,data)
 
-        data.append(lat)
-        data.append(long)
-        
 
     else:
         error = 'Error with HTTP response'
@@ -64,15 +63,37 @@ def weather_finder(city,country_code="",state_code="",limit=1):
 @bp.route('/display')
 def weather_displayer():
     #instantly renders weather.html, and i display the code here
-    long = request.args.get('long', 'No Data Available')
-    lat = request.args.get('lat', 'No Data Available')
+    name = request.args.get('name', 'No Data Available')
+    temp = request.args.get('temp', 'No Data Available')
+    feels_like = request.args.get('feels_like', 'No Data Available')
+    pressure = request.args.get('temp', 'No Data Available')
+    humidity = request.args.get('humidity', 'No Data Available')
+    
+    
+    
+    
+    return render_template('weather/weather.html',name=name, temp=temp, feels_like=feels_like, pressure=pressure,humidity=humidity)
 
-    print(lat,long)
-    return render_template('weather/weather.html',long=long,lat=lat)
-
+    
+def current_weather(lat,lon,API_key,data):
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        print("Successfully made a request to openweathermap")
+        print(response.content)
+        data_list = response.json()
+        main = data_list['main']
+        print(main)
+        #get all relevent data
+        temp,feels_like,pressure,humidity = main['temp'],main['feels_like'], main['pressure'], main['humidity']
+        data.append(temp)
+        data.append(feels_like)
+        data.append(pressure)
+        data.append(humidity)
     
         
-        
     
-    
-    
+@bp.route('/searches', methods = ('GET', 'POST'))
+def searches():
+    return render_template('weather/recent.html')
