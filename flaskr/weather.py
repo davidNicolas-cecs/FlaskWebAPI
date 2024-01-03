d747c9fc5585c7acd6da1ff061057247a15d05fc
davidNicolas-cecs <david.nicolasgarcia@student.csulb.edu>
2024-01-03 01:32:34 -0800

    Upgrades weather lookup to include check boxes for user requested data as well as more validation for user input

--- flaskr/weather.py
+++ flaskr/weather.py
@@ -27,11 +27,25 @@ def index():
     error = None
     
     if request.method == 'POST':
+        #take data from 'index.html
         city = request.form['city']  
         state_code = request.form['state_code']
+        country_code = request.form['country_code']
+        
+        #requested data
+        want_humidity = request.form.get('humidity') # returns string
+        want_pressure = request.form.get('pressure')
+        want_feels_like = request.form.get('feels_like')
+        print(f"Humidity is {want_humidity}, pressure is {want_pressure}, feels like is {want_feels_like}")
+        
+        #Checks if all the values entered are correct, if not, redirect back to home
+        if not checkValues(city,state_code,country_code):
+            print("Invalid request forms, redirecting back to home page---")
+            return redirect(url_for('weather.index'))
+            
         #try getting lat and lon from info
         try: 
-            data = geocoding(city,state_code=state_code)
+            data = geocoding(city,state_code,country_code,want_humidity,want_pressure,want_feels_like)
         except:
             flash("Could not find the location, Please try again.")
             return redirect(url_for("weather.index"))
@@ -46,7 +60,6 @@ def index():
                     user_id_query = f"SELECT id FROM user WHERE username = '{g.user['username']}'"
                     user_id_result = db.execute(user_id_query)
                     user_id = user_id_result.fetchone()[0] if user_id_result else None
-                    print(user_id)
                 except:
                     flash("Error retrieving recent searches")
                     return redirect(url_for('weather.index'))
@@ -65,10 +78,26 @@ def index():
             return redirect(url_for('weather.index'))
     return render_template('weather/index.html')
 
+def checkValues(city,state_code,country_code):
+    '''
+    Description:
+    -------------
+    Function validates the user input from index.html
+    
+    Gets: data from index.html and verifys that it is in correct format
+    
+    Returns: true or false
+    '''
+    #returns false if a value is incorrect
+    error = "Invalid format"
+    if city.isalpha() and (len(state_code)) < 4 and (state_code.isalpha or state_code == "" ) and len(country_code) < 7:
+        return True   
+    flash(error)
+    return False
 
 
 
-def geocoding(city,country_code="",state_code="",limit=1):
+def geocoding(city,state_code,country_code,want_humidity,want_pressure,want_feels_like):
     '''
     Description:
     -------------
@@ -78,6 +107,7 @@ def geocoding(city,country_code="",state_code="",limit=1):
     
     Returns: data as a list or false if location not found
     '''
+    limit=1
     API_key = "696ff3236cff2b889401a9a8d64c86c2"    
     url = f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state_code},{country_code}&limit={limit}&appid={API_key}"
     response = requests.get(url)
@@ -89,9 +119,9 @@ def geocoding(city,country_code="",state_code="",limit=1):
         first_item = data_list[0]
         name = first_item['name']
         lat = first_item["lat"]
-        long = first_item["lon"]
+        lon = first_item["lon"]
         data.append(name)
-        current_weather(lat,long,API_key,data)
+        current_weather(lat,lon,API_key,data,want_humidity,want_pressure,want_feels_like)
     else:
         error = f'Could not find the location{response.status_code}'
         flash(error)  
@@ -113,22 +143,22 @@ def weather_displayer():
     name = request.args.get('name', 'No Data Available')
     temp = request.args.get('temp', 'No Data Available')
     feels_like = request.args.get('feels_like', 'No Data Available')
-    pressure = request.args.get('temp', 'No Data Available')
+    pressure = request.args.get('pressure', 'No Data Available')
     humidity = request.args.get('humidity', 'No Data Available')
     return render_template('weather/weather.html',name=name, temp=temp, feels_like=feels_like, pressure=pressure,humidity=humidity)
 
     
-def current_weather(lat,lon,API_key,data):
+def current_weather(lat,lon,API_key,data,want_humidity,want_pressure,want_feels_like):
     '''
     Description:
     -------------
     Function gets the current weather from the geolocations provided 
     
-    Gets: data from openweatherapi (temp, feels_like, humidity, pressure)
+    Gets: data from openweatherapi (temp, feels_like, humidity, pressure) as well as user requested data
     
-    Returns: data as a list
+    Returns: data as a list with the specified users requested data only
     '''
-    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}"
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid={API_key}"
     response = requests.get(url)
     
     if response.status_code == 200:
@@ -137,10 +167,32 @@ def current_weather(lat,lon,API_key,data):
         main = data_list['main']
         #get all relevent data
         temp,feels_like,pressure,humidity = main['temp'],main['feels_like'], main['pressure'], main['humidity']
+        
         data.append(temp)
-        data.append(feels_like)
-        data.append(pressure)
-        data.append(humidity)
+        
+        print("CHECKING BOXES ON USER WANTS")
+        
+        if want_feels_like == 'on':
+            print("we want feels like")
+            data.append(feels_like)
+        else:
+            print("we dont want feels like")
+            data.append('off')
+            
+        if want_pressure == 'on':
+            print('we want pressure')
+            data.append(pressure)
+        else:
+            print('we dont want pressure')
+            data.append('off')
+        if want_humidity == 'on':
+            print('we want humidity')
+            data.append(humidity)
+        else:
+            print('we dont want humidity')
+            data.append('off')       
+            
+        print(data)
     
         
     
